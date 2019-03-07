8de3afc5e068dc1f511b4ef2c82ac9961acdec8e
David Jaquet <djaquet5@gmail.com>
2019-03-07 14:41:24 +0100

    First commit

---
+++ cstring.cpp
@@ -0,0 +1,14 @@
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : 02
+ Fichier     : cstring.h
+ Auteur(s)   : David Jaquet & Christoph Rueff
+ Date        : 07.03.2019
+ But         : Implémentation de la classe String
+
+ Remarque(s) :
+
+ -----------------------------------------------------------------------------------
+*/
+
+#include "cstring.h"
---
+++ cstring.h
@@ -0,0 +1,20 @@
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : 02
+ Fichier     : cstring.h
+ Auteur(s)   : David Jaquet & Christoph Rueff
+ Date        : 07.03.2019
+
+ Remarque(s) :
+
+ -----------------------------------------------------------------------------------
+*/
+
+#pragma once
+
+/**
+ * Spécification de la classe String
+ */
+class String {
+
+};
