c5da499074f1cb283022b4d3e242c7e7f89eb9a0
David Jaquet <djaquet5@gmail.com>
2019-03-07 15:11:23 +0100

    Add files

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : 02
+ Fichier     : main.cpp
+ Auteur(s)   : David Jaquet & Christoph Rueff
+ Date        : 07.03.2019
+
+ But         : Programme de testant l'implémentation d'une classe String
+
+ Remarque(s) :
+
+ -----------------------------------------------------------------------------------
+*/
+
+#include <iostream>
+
+/**
+ * Programme de test
+ *
+ * @return Indique si le programme s'est déroulé avec ou sans problème
+ */
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    return EXIT_SUCCESS;
+}
